9f7263bf3974497062685ab967991670c2a26642
Van Nguyen <van.nguyenthi@asnet.com.vn>
2022-11-24 15:05:42 +0700

    example

--- demo-2/middleware.ts
+++ demo-2/middleware.ts
@@ -1,17 +1,15 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname
-  if (pathname && pathname.replaceAll('/', '').length === 8) {
-    const code = pathname.replaceAll('/', '');
-    // typeof window !== 'undefined' && localStorage.setItem('code', code);
-    return NextResponse.redirect(new URL(`/:${code}`, request.url), 308)
-  }
-}
+  const pathname = request.nextUrl.pathname;
+  const code = pathname ? pathname.replaceAll('/', '') : '';
 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: '/:code*',
+  if (code.length === 8 && code !== 'referral') {
+    return NextResponse.redirect(
+      new URL(`/referral?code=${code}`, request.url),
+      308
+    );
+  }
 }
